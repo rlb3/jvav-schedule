a020bf2c65659b00c1a693d2d93fd9dfd35d6076
Robert Boone <robert@rlb3.com>
2015-08-21 17:59:44 -0500

    Add all fields to schedule model

--- app/models/schedule.js
+++ app/models/schedule.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   scheduledDate: DS.attr('date'),
   audio: DS.attr('string'),
+  audioOff: DS.attr('string'),
   video: DS.attr('string'),
-  alternate: DS.attr('string')
+  videoOff: DS.attr('string'),
+  alternate: DS.attr('string'),
+  alternateOff: DS.attr('string')
 });
